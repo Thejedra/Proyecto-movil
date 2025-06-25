1dcfd76483b4f098fa3b05beb0c0a30e938f348b
Thejedra <quevedojetzael@gmail.com>
2025-06-25 00:00:14 -0600

    Mejoras 25/06/2025

--- src/app/iniciosesion/iniciosesion.component.ts
+++ src/app/iniciosesion/iniciosesion.component.ts
@@ -2,27 +2,39 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-iniciosesion',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, HttpClientModule],
   templateUrl: './iniciosesion.component.html',
   styleUrl: './iniciosesion.component.css'
 })
 export class IniciosesionComponent {
   usuario: string = '';
-  rol: string = '';
+  password: string = '';
+  error: string = '';
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, private http: HttpClient) {}
 
   iniciarSesion() {
-  if (this.rol === 'mesero') {
-    this.router.navigate(['/mesas']);
-  } else if (this.rol === 'gerente') {
-    this.router.navigate(['/admin/mesas']);
-  } else {
-    alert('Seleccione un rol válido.');
+    this.http.post<any>('http://localhost:3000/api/login', {
+      usuario: this.usuario,
+      password: this.password
+    }).subscribe(
+      res => {
+        if (res.rol === 'mesero') {
+          this.router.navigate(['/mesas']);
+        } else if (res.rol === 'gerente') {
+          this.router.navigate(['/admin/mesas']);
+        } else {
+          this.error = 'Rol no válido';
+        }
+      },
+      err => {
+        this.error = 'Usuario o contraseña incorrectos';
+      }
+    );
   }
-}
 }
